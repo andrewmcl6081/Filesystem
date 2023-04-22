6a5bc7801f04054684e27ddcda7da941ee1ddb01
Andrew McLaughlin <andrewmcl6081@gmail.com>
2023-04-22 13:13:52 -0500

    Added parser for input

--- src/msf.c
+++ src/msf.c
@@ -25,7 +25,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/wait.h>
+//#include <sys/wait.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
@@ -51,39 +51,7 @@ void store_history(int* pid_history, char** history, int* curr_history_index, ch
 
 int main()
 {
-
   char * command_string = (char*) malloc( MAX_COMMAND_SIZE );
-
-  // temp string to tokenize command_string
-  char cmd_str_cpy[255];
-
-  // temp string to tokenize !n functionality
-  char temp_string[255];
-
-  // token after tokenizing !n
-  char* temp_token;
-
-  // int to store !(n)
-  int n = 0;
-
-  // the current # of items in our history
-  int curr_hist_index = 0;
-
-  // array to store pids
-  int pid_history[MAX_HIST_STORED];
-
-  // array of pointers to store command history
-  char* history[MAX_HIST_STORED];
-
-  pid_t pid;
-
-  // initialize history array to NULL
-  for(int i = 0; i < MAX_HIST_STORED; i++)
-  {
-    history[i] = NULL;
-  }
-
-
 
 
   while( 1 )
@@ -98,29 +66,7 @@ int main()
     // is no input
     while( !fgets (command_string, MAX_COMMAND_SIZE, stdin) );
 
-    // if we are utilizing "!n" functionality
-    // tokenize the command string, check if n is
-    // valid in history, and if it is copy its value to
-    // command string. If it is not continue and prompt for
-    // next input.
-    if((command_string[0] == '!'))
-    {
-      strcpy(temp_string, command_string);
-      temp_token = strtok(temp_string, "!");
-      n = atoi(temp_token);
-
-      if( (n >= 0) && (n < 15) && (n < curr_hist_index) )
-      {
-        strcpy(command_string, history[n]);
-      }
-      else
-      {
-        printf("Command not in history.\n");
-        continue;
-      }
-
-    }
-
+   
     /* Parse input */
     char *token[MAX_NUM_ARGUMENTS];
 
@@ -155,97 +101,8 @@ int main()
     }
 
 
-    // Remove newline from command string
-    // There may or may not be one depending
-    // If "!n" has been processed
-    strcpy(cmd_str_cpy, command_string);
-    char* modified_string = strtok(cmd_str_cpy, "\n");
 
-    
-    // Blank line input will continue for another prompted input
-    if(token[0] == NULL)
-    {
-      free(head_ptr);
-      continue;
-    }
-    // Exit or quit will free all memory for a graceful exit
-    else if(!strcmp("exit", token[0]) || !strcmp("quit", token[0]))
-    {
-      for( int i = 0; i < MAX_NUM_ARGUMENTS; i++ )
-      {
-        if( token[i] != NULL )
-        {
-          free( token[i] );
-        }
-      }
-
-      free(head_ptr);
-      free(command_string);
-      clear_history(history);
-      
-      exit(EXIT_SUCCESS);
-    }
-    // If user inputs history or history -p store command and execute proper choice
-    else if(!strcmp("history", token[0]))
-    {
-      store_history(pid_history,history, &curr_hist_index, modified_string, -1);
-
-      if( (token[1] != NULL) && (!strcmp("-p", token[1])) )
-      {
-        print_history_pids(pid_history,history,&curr_hist_index);
-      }
-      else
-      {
-        print_history(history,&curr_hist_index);
-      }
-    }
-    // Store command and pid in history and display previous pids
-    else if(!strcmp("showpids", token[0]))
-    {
-      store_history(pid_history,history, &curr_hist_index, modified_string, -1);
-
-      print_pids(pid_history, &curr_hist_index);
-    }
-    // Store command in history and change directories if directory is valid
-    else if(!strcmp("cd", token[0]))
-    {
-      store_history(pid_history,history, &curr_hist_index, modified_string, -1);
-
-      int ret = chdir(token[1]);
-
-      if(ret == -1)
-      {
-        printf("msh: %s: %s: No such file or directory\n", token[0], token[1]);
-      }
-    }
-    // If our desired command isnt handled by parent fork a new child and execute
-    // and store to history
-    else
-    {
-      pid = fork();
-
-      if(pid == -1)
-      {
-        exit(EXIT_FAILURE);
-      }
-      else if(pid == 0)
-      {
-        int ret = execvp(token[0], &token[0]);
-
-        if(ret == -1)
-        {
-          printf("%s: Command not found.\n", token[0]);
-          exit(EXIT_FAILURE);
-        }
-      }
-      else
-      {
-        int status;
-        wait(&status);
-
-        store_history(pid_history,history, &curr_hist_index, modified_string, pid);
-      }
-    }
+   
 
     // Cleanup allocated memory
     for( int i = 0; i < MAX_NUM_ARGUMENTS; i++ )
@@ -264,67 +121,4 @@ int main()
 
   return 0;
   // e2520ca2-76f3-90d6-0242ac120003
-}
-
-// Clear history to avoid segfaults
-void clear_history(char* history[])
-{
-  for(int i = 0; i < MAX_HIST_STORED; i++)
-  {
-    free(history[i]);
-  }
-}
-
-// Print out just the pids to stdout
-void print_pids(int* pid_history, int* curr_hist_index)
-{
-  for(int i = 0; i < *curr_hist_index; i++)
-  {
-    printf("%d: %d\n", i, pid_history[i]);
-  }
-}
-
-// Display history of all previously entered commands
-void print_history(char** history, int* curr_hist_index)
-{
-  for(int i = 0; i < *curr_hist_index; i++)
-  {
-    printf("%d: %s\n", i, history[i]);
-  }
-}
-
-// Display history with respective pids for "history -p"
-void print_history_pids(int* pid_history, char** history, int* curr_hist_index)
-{
-  for(int i = 0; i < *curr_hist_index; i++)
-  {
-    printf("%d: %s PID: %d\n", i, history[i], pid_history[i]);
-  }
-}
-
-// Store history of commands and pids for future use
-// and handle only 15 commands kicking out the oldest
-// command and storing a new command 
-void store_history(int* pid_history, char** history, int* curr_hist_index, char* modified_string, int pid)
-{
-  if(*curr_hist_index < MAX_HIST_STORED)
-    {
-      history[*curr_hist_index] = strdup(modified_string);
-      pid_history[*curr_hist_index] = pid;
-
-      *curr_hist_index = *curr_hist_index + 1;
-    }
-    else
-    {
-      free(history[0]);
-
-      for(int i = 1; i < MAX_HIST_STORED; i++)
-      {
-        history[i-1] = history[i];
-        pid_history[i-1] = pid_history[i];
-      }
-
-      history[MAX_HIST_STORED - 1] = strdup(modified_string);
-      pid_history[MAX_HIST_STORED - 1] = pid;
-    }
 }
