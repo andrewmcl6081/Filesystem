5d7bb19ccdaf7142ec0c6e935970a8751b12506d
Andrew McLaughlin <89934621+andrewmcl6081@users.noreply.github.com>
2023-04-22 19:31:22 +0000

    Added createfs command

--- src/msf.c
+++ src/msf.c
@@ -1,35 +1,46 @@
-// The MIT License (MIT)
-// 
-// Copyright (c) 2016 Trevor Bakker 
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 #define _GNU_SOURCE
 
 #include <stdio.h>
 #include <unistd.h>
-//#include <sys/wait.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
+#include <stdint.h>
+
+#define BLOCK_SIZE 1024
+#define NUM_BLOCKS 65536
+#define BLOCKS_PER_FILE 1024
+#define NUM_FILES 256
+
+uint8_t data[NUM_BLOCKS][BLOCK_SIZE];
+
+
+// directory
+struct directoryEntry
+{
+  char    filename[64];
+  short   in_use;
+  int32_t inode; //max inode
+};
+
+struct directoryEntry* directory;
+
+// inode
+struct inode
+{
+  int32_t blocks[BLOCKS_PER_FILE];
+  short   in_use;
+};
+
+struct inode* inodes;
+
+FILE* fp;
+
+
+
+
 
 #define WHITESPACE " \t\n"      // We want to split our command line up into tokens
                                 // so we need to define what delimits our tokens.
@@ -42,22 +53,61 @@
 
 #define MAX_HIST_STORED 15   //15   // Maximum number of commands to store in history
 
+void init()
+{
+  //directory pointer will point to the beginning of our directory (blocks 0-18)
+  directory = (struct directoryEntry*) &data[0][0];
 
-void clear_history(char* history[]);
-void print_pids(int* pid_history, int* curr_history_index);
-void print_history(char** history, int* curr_hist_index);
-void print_history_pids(int* pid_history, char** history, int* curr_hist_index);
-void store_history(int* pid_history, char** history, int* curr_history_index, char* modified_string, int pid);
+  //inodes will point to beginning of our inodes (blocks 20-276)
+  inodes = (struct inode*) &data[20][0];
+
+  int i;
+  for(i = 0; i < NUM_FILES; i++)
+  {
+    //setting the directory entries to available
+    directory[i].in_use = 0;
+
+    //setting each directory entry's inode to -1
+    directory[i].inode = -1;
+
+    memset(directory[i].filename, 0, 64);
+
+    int j;
+    for(j = 0; j < NUM_BLOCKS; j++)
+    {
+      //setting the blocks within inodes to -1
+      inodes[i].blocks[j] = -1;
+
+      //setting the inodes to be available
+      inodes[i].in_use = 0;
+    }
+  }
+}
+
+
+void createfs(char* filename)
+{
+  fp = fopen(filename, "r");
+
+  memset(data, 0, NUM_BLOCKS * BLOCK_SIZE);
+
+  fclose(fp);
+}
+
 
 int main()
 {
   char * command_string = (char*) malloc( MAX_COMMAND_SIZE );
 
+  fp = NULL;
+
+  init();
+
 
   while( 1 )
   {
     // Print out the msh prompt
-    printf ("msh> ");
+    printf ("msf> ");
 
     // Read the command from the commandline.  The
     // maximum command that will be read is MAX_COMMAND_SIZE
@@ -98,6 +148,28 @@ int main()
         token[token_count] = NULL;
       }
         token_count++;
+    }
+
+
+    //handle blank line input
+    if(token[0] == NULL)
+    {
+      free(head_ptr);
+      continue;
+    }
+
+
+
+    // process the filesystem commands
+    if(!strcmp("createfs", token[0]))
+    {
+      if(token[1] == NULL)
+      {
+        printf("ERROR: No filename specified\n");
+        continue;
+      }
+
+      createfs(token[1]);
     }
 
 
