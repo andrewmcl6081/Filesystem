e709b9d22905b4d5464e264a94871650d7e03ac7
Andrew McLaughlin <89934621+andrewmcl6081@users.noreply.github.com>
2023-04-26 00:20:48 +0000

    Added findfreeBlock and findFreeInode functions

--- mfs.c
+++ mfs.c
@@ -8,12 +8,14 @@
 #include <string.h>
 #include <signal.h>
 #include <stdint.h>
+#include <sys/stat.h>
 
 #define BLOCK_SIZE 1024
 #define NUM_BLOCKS 65536
 #define BLOCKS_PER_FILE 1024
 #define NUM_FILES 256
 #define FIRST_DATA_BLOCK 790
+#define MAX_FILE_SIZE 1048576
 
 uint8_t data[NUM_BLOCKS][BLOCK_SIZE];
 
@@ -35,9 +37,10 @@ struct directoryEntry* directory;
 // inode
 struct inode
 {
-  int32_t blocks[BLOCKS_PER_FILE];
-  short   in_use;
-  uint8_t attribute;
+  int32_t  blocks[BLOCKS_PER_FILE];
+  short    in_use;
+  uint8_t  attribute;
+  uint32_t file_size;
 };
 
 struct inode* inodes;
@@ -59,6 +62,37 @@ uint8_t image_open;
 #define MAX_NUM_ARGUMENTS 11  //11   // Mav shell only supports four arguments
 
 #define MAX_HIST_STORED 15   //15   // Maximum number of commands to store in history
+
+int32_t findFreeBlock()
+{
+  int i;
+  for( i = 0; i < NUM_BLOCKS; i++)
+  {
+    if(free_blocks[i])
+    {
+      return i + 790;
+    }
+  }
+
+  return -1;
+}
+
+
+int32_t findFreeInode()
+{
+  int i;
+  for(i = 0; i < NUM_FILES; i++)
+  {
+    if(free_inodes[i])
+    {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+
 
 void init()
 {
@@ -89,19 +123,15 @@ void init()
 
     memset(directory[i].filename, 0, 64);
 
+
+    //initialize our inodes (20 - 276)
     int j;
     for(j = 0; j < NUM_BLOCKS; j++)
     {
-      //setting the blocks within inodes to -1
       inodes[i].blocks[j] = -1;
-
-      //setting the inodes to be available
       inodes[i].in_use = 0;
-
-      //setting attribute init value
       inodes[i].attribute = 0;
-
-
+      inodes[i].file_size = 0;
     }
   }
 
@@ -114,7 +144,7 @@ void init()
 }
 
 
-void df()
+uint32_t df()
 {
   int j;
   int count = 0;
@@ -126,7 +156,7 @@ void df()
     }
   }
 
-  printf("%d bytes free\n", count * BLOCK_SIZE);
+  return count * BLOCK_SIZE;
 }
 
 
@@ -227,6 +257,144 @@ void list()
   }
 }
 
+
+//insert a file into system
+void insert(char* filename)
+{
+  //verify filename is not NULL
+  if(filename == NULL)
+  {
+    printf("ERROR: Filename is Null\n");
+  }
+
+  //verify the file exists
+  struct stat buf;
+  int ret = stat(filename, &buf);
+
+  if(ret == -1)
+  {
+    printf("ERROR: File does not exist\n");
+    return;
+  }
+
+  //verify the file is not too big
+  if(buf.st_size > MAX_FILE_SIZE)
+  {
+    printf("ERROR: File is too large\n");
+    return;
+  }
+
+  //verify there is enough space
+  if(buf.st_size > df())
+  {
+    printf("ERROR: Not enough free disk space\n");
+    return;
+  }
+
+  //find an empty directory
+  int i;
+  int directory_entry = -1;
+  for( i = 0; i < NUM_FILES; i++)
+  {
+    if(directory[i].in_use == 0)
+    {
+      directory_entry = i;
+      break;
+    }
+
+  }
+
+  if(directory_entry == -1)
+  {
+    printf("ERROR: Could not find a free directory entry\n");
+    return;
+  }
+
+  // Open the input file read-only
+  FILE* ifp = fopen(filename, "r");
+  printf("Reading %d bytes from %s\n", (int) buf.st_size, filename);
+
+  // Save off the size of the input file since we'll use it in a couple of places and
+  // also initialize our index variables to zero
+  int32_t copy_size = buf.st_size;
+
+  // We want to copy and write in chunks of BLOCK_SIZE. So to do this
+  // we are going to use fseek to move along our file stream in chunks of BLOCK_SIZE.
+  // We will copy bytes, increment our file pointer by BLOCK_SIZE and repeat.
+  int32_t offset = 0;
+
+  // We are going to copy and store our file in BLOCK_SIZE chunks instead of one big
+  // memory pool. Why? We are simulating the way the file system stores file data in
+  // blocks of space on the disk. block_index will keep us pointing to the area that
+  // we will read from or write to.
+  int32_t block_index = -1; //using our block map to find a free block
+
+  // copy_size is initialized to the size of the input file so each loop iteration we
+  // will copy BLOCK_SIZE bytes from the file then reduce our copy_size counter by
+  // BLOCK_SIZE number of bytes. When copy_size is less than or equal to zero we know
+  // we have copied all the data from the input file
+  while(copy_size > 0)
+  {
+
+    // Index into the input file by offset number of bytes. Initially offset is set to
+    // zero so we copy BLOCK_SIZE number of bytes from the front of the file. We
+    // then increase the offset by BLOCK_SIZE and continue the process. This will
+    // make us copy from offset 0, BLOCK_SIZE, 2*BLOCK_SIZE, 3*BLOCK_SIZE, etc.
+    fseek(ifp, offset, SEEK_SET);
+
+    // Read BLOCK_SIZE number of bytes from the input file and store them in our
+    // data array.
+
+    // find a free block
+    block_index = findFreeBlock();
+
+    if(block_index == -1)
+    {
+      printf("ERROR: Can not find a free block\n");
+      return;
+    }
+
+    // find a free inode
+    int32_t inode_index = findFreeInode();
+
+    if(inode_index == -1)
+    {
+      printf("ERROR: Can not find a free inode\n");
+      return;
+    }
+
+    // place the file info in the directory
+    directory[directory_entry].in_use = 1;
+
+    int32_t bytes = fread(data[block_index], BLOCK_SIZE, 1, ifp);
+
+    // If bytes == 0 and we haven't reached the end of the file then something is
+    // wrong. If 0 is returned and we also have the EOF flag set then that is OK.
+    // It means we've reached the end of our input file.
+    if( bytes == 0 && !feof(ifp))
+    {
+      printf("An error occured reading from the input file\n");
+      return -1;
+    }
+
+    // Clear the EOF file flag
+    clearerr(ifp);
+
+    // Reduce copy_size by the BLOCK_SIZE bytes
+    copy_size -= BLOCK_SIZE;
+
+    // Increase the offset into our input file by BLOCK_SIZE. This will allow
+    // the fseek at the top of the loop to position us to the correct spot.
+    offset += BLOCK_SIZE;
+
+    // Increment the index into the block array
+    // Do NOT just increment block index in your filesystem
+    block_index++;
+  } 
+
+  // We are done copying from the input file so close it out
+  fclose(ifp);
+}
 
 int main()
 {
@@ -357,7 +525,33 @@ int main()
         continue;
       }
 
-      df();
+      printf("%d bytes free\n", df());
+    }
+
+
+    //quit
+    if(!strcmp("quit", token[0]))
+    {
+      exit(0);
+    }
+
+
+    //insert
+    if(!strcmp("insert", token[0]))
+    {
+      if(!image_open)
+      {
+        printf("ERROR: Disk image is not open\n");
+        continue;
+      }
+
+      if(token[1] == NULL)
+      {
+        printf("ERROR: No filename specified\n");
+        continue;
+      }
+
+      insert(token[1]);
     }
 
    
